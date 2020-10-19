2b71d6c3a47364a8d2e3452c90ab03f15fd9fba1
Jasper M-W <jasper@ngita.co.nz>
2020-10-19 21:17:01 +1300

    Mock start and Gatsby config

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../components/Header";
+
+import FallstopLogo from "../assets/image/FallstopLogo.svg";
+
+export const Home = () => (
+  <div className="home">
+      <Header />
+      Hello world!
+      
+    </div>
+);
+
+
+
