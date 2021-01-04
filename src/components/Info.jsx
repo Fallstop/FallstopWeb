734bc96488af0a631777dd2624e55ab5e1c0c77f
Jasper M-W <jasper@ngita.co.nz>
2021-01-04 21:44:38 +1300

    Start of about me

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -49,9 +49,39 @@ const ProjectContainer = () => {
 const AboutMe = () => {
 	return <div className="aboutMe">
 		<span className="basicPersonalInformation">
-			{/* I am a make-believe software developer that likes to experiment in new technologies. I am currently studying in Year 11 in Huanui Collage and contract part time for website design/various other things. I am also a full time member of <a href="https://questionable.org.nz" style={{color: "#007acc"}}>Questionable Research Labs</a>, and you might also find many of my projects are supported by them and their members. */}
-			Yes TD.
+			I am a software developer/stack overflow expert that likes to experiment in new technologies.
+			<br/>
+			I am currently studying in Year 11 in Huanui Collage and contract part time for website design/various other things. I am also a full time member of <a href="https://questionable.org.nz" style={{color: "#007acc"}} target="__none">Questionable Research Labs</a>, and you might also find many of my projects are supported by them and their members.
 		</span>
+		<div className="aboutLinks">
+			<a href="/" target="__none">
+				<div className="aboutLinkItem">
+					<div className="aboutLinkName">Email</div>
+					<div className="aboutLinkIcon"></div>
+				</div>
+			</a>
+			<a href="https://github.com/Fallstop" target="__none">
+				<div className="aboutLinkItem">
+					<div className="aboutLinkName">Github</div>
+					<div className="aboutLinkIcon"></div>
+				</div>
+			</a>
+			<a href="/" target="__none">
+				<div className="aboutLinkItem">
+					<div className="aboutLinkName">Discord</div>
+					<div className="aboutLinkIcon"></div>
+				</div>
+			</a>
+			
+		</div>
+		<div className="aboutLinks">
+			<a href="/skills">
+				<div className="aboutLinkItem">
+					<div className="aboutLinkName">Skills</div>
+					<div className="aboutLinkIcon"></div>
+				</div>
+			</a>
+		</div>
 	</div>
 }
 
