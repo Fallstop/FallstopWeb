536d5193ddfb58898a2b48c8f139ac0251ea000c
Jasper M-W <jasper@ngita.co.nz>
2021-03-09 18:38:21 +1300

    Potential Speed increases around font

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,5 @@ import React from 'react'
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-    <link key="fonts" href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;700&family=Open+Sans&display=swap" rel="stylesheet"></link>
   ])
 }
