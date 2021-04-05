bd04ccbaee6b52cf7e6b8d59528d52d0eb3b70cc
Jasper M-W <jasper@ngita.co.nz>
2021-04-05 22:44:57 +1200

    Updated SEO discriptions

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -7,6 +7,7 @@ export function SearchEngineOptimization (props) {
   console.log(props);
   let title = (props.titleExt !== undefined) ? 'Jasper M-W | '  + props.titleExt : 'Jasper M-W';
   console.log((props.description !== undefined) ? props.description : "Jasper Miller-Waugh's (Fallstop) portfolio");
+
   let description = (props.description !== undefined) ? props.description : "Jasper Miller-Waugh's (Fallstop) portfolio";
   return (
     <div>
@@ -15,8 +16,8 @@ export function SearchEngineOptimization (props) {
         description={description}
         language="en"
         openGraph={{
-          title: 'Jasper M-W/Fallstop | Portfolio',
-          description: 'Jasper M-W | ' + description,
+          title: title,
+          description: description,
           type: 'profile',
           profile: {
             firstName: 'Jasper',
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -7,18 +7,18 @@ import '../styles/404.scss';
 export default function Home() {
 	return (
 		<>
-		<SearchEngineOptimization/>
+			<SearchEngineOptimization titleExt="404" description="What did you expect, it's a 404 page." />
 			<StandardLayout className="NotFoundErrorPage">
 				<div className="title">404</div>
 				<div className="subtitle">PAGE NOT FOUND</div>
 				<div className="definition">
-					<span className="content">"The requested resource could not be found but may be available in the future. Subsequent requests by the client are permissible."</span><br/>
+					<span className="content">"The requested resource could not be found but may be available in the future. Subsequent requests by the client are permissible."</span><br />
 					<span className="attribution">-Wikipedia, {new Date().getFullYear()}</span>
 				</div>
 				<div className="homeWrapper">
 					<a className="goBackToHomeButton" href="/"><div>Want to go back to home?</div></a>
 				</div>
-				
+
 			</StandardLayout>
 		</>
 	);
