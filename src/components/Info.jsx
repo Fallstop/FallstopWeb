2c43f86bd47a941f92a673bb1aa446a309b51d73
Jasper M-W <jasper@qrl.nz>
2021-06-07 17:27:29 +1200

    Accidental Author and reworked project archive

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,27 +3,45 @@ import { useStaticQuery, graphql, Link } from 'gatsby';
 
 import { MailIcon, DiscordIcon, GithubIcon, ToolsIcon } from './SVGS';
 
+
+const ProjectArchiveButton = (props) => {
+	console.log('Project Count Query', props.ProjectCountQuery);
+	if (props.ProjectCountQuery > 6) {
+		return <div className="projectSeeMore">
+			<Link to="/archive" about="Go to the Project Archive Page">
+				{props.ProjectCountQuery - 6} More Project{(props.ProjectCountQuery > 7) ? "s" : ""}
+			</Link>
+		</div>;
+	} else {
+		return <></>;
+	}
+
+};
+
 const ProjectContainer = () => {
 	let projectElements = [];
-	const result = useStaticQuery(
+	const MarkdownFiles = useStaticQuery(
 		graphql` {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          limit: 6
-        ) {
-          edges {
-            node {
-              frontmatter {
-                slug
-				title
-              }
-            }
-          }
+			allMarkdownRemark(
+			sort: { order: DESC, fields: [frontmatter___date] }
+			limit: 6
+			) {
+				totalCount
+				edges {
+					node {
+					frontmatter {
+						slug
+						title
+					}
+					}
+				}
         }
       }
 	`);
+	const ProjectCountQuery = MarkdownFiles.allMarkdownRemark.totalCount;
 
-	result.allMarkdownRemark.edges.forEach(({ node }) => {
+
+	MarkdownFiles.allMarkdownRemark.edges.forEach(({ node }) => {
 		projectElements.push(
 			<Link to={node.frontmatter.slug} key={node.frontmatter.slug}>
 				<article className="projectBox" >
@@ -31,20 +49,17 @@ const ProjectContainer = () => {
 				</article>
 			</Link>
 
-		)
+		);
 	});
-
 
 	return <div className="projectsContainer">
 		<div className="projectsFlex">
 			{projectElements}
 		</div>
 
-		<div className="projectSeeMore">
-			<Link to="/archive" about="Go to the Project Archive Page">Project Archive</Link>
-		</div>
-	</div>
-}
+		<ProjectArchiveButton ProjectCountQuery={ProjectCountQuery} />
+	</div>;
+};
 
 const AboutMe = () => {
 	return <div className="aboutMe">
@@ -58,7 +73,7 @@ const AboutMe = () => {
 						<div className="aboutLinkItem">
 							<span className="aboutLinkName">Email - <span className="aboutLinkNameSubtext">jasper@qrl.nz</span></span>
 							<span className="aboutLinkIcon">
-								<MailIcon/>
+								<MailIcon />
 							</span>
 						</div>
 					</a>
@@ -66,7 +81,7 @@ const AboutMe = () => {
 						<div className="aboutLinkItem">
 							<span className="aboutLinkName">Github - <span className="aboutLinkNameSubtext">Fallstop</span></span>
 							<span className="aboutLinkIcon">
-								<GithubIcon/>
+								<GithubIcon />
 							</span>
 						</div>
 					</a>
@@ -74,7 +89,7 @@ const AboutMe = () => {
 						<div className="aboutLinkItem">
 							<span className="aboutLinkName">Discord - <span className="aboutLinkNameSubtext">fallstop#3106</span></span>
 							<span className="aboutLinkIcon">
-								<DiscordIcon/>
+								<DiscordIcon />
 							</span>
 						</div>
 					</a>
@@ -84,17 +99,17 @@ const AboutMe = () => {
 			<div className="aboutPersonalInformation">
 				<h3 className="aboutPersonalHeader">Confirmed Bot</h3>
 				<div className="aboutPersonalSubheader">I am a software developer/stack overflow expert that likes to experiment in new technologies.</div>
-				
+
 				I am currently studying in Huanui Collage (year eleven) and contract part time for website design/various other things. I am also a full time
 				member of <a href="https://questionable.org.nz" style={{ color: "#007acc" }} target="__none">Questionable Research Labs</a>,
 				and you might also find many of my projects are supported by them and their members.
-				<br/><br/>
+				<br /><br />
 				But that is enough about my life, the real question is what tools I use, and here is the answers:
 				<div className="aboutLinks centerAlign">
 					<Link to="/tools">
 						<div className="aboutLinkItem">
 							<span className="aboutLinkName">Tools</span>
-							<span className="aboutLinkIcon"><ToolsIcon/></span>
+							<span className="aboutLinkIcon"><ToolsIcon /></span>
 						</div>
 					</Link>
 				</div>
@@ -103,8 +118,8 @@ const AboutMe = () => {
 		</div>
 
 
-	</div>
-}
+	</div>;
+};
 
 const InfoContainer = () => {
 	return (
@@ -115,6 +130,6 @@ const InfoContainer = () => {
 				<h2 className="header" id="aboutMeSection">About Me</h2>
 				<AboutMe />
 			</div>
-		</div>)
+		</div>);
 };
 export default InfoContainer;
