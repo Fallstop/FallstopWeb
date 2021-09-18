614ffacecb67aa837517e43745d82e383fa827e0
Jasper Miller-Waugh <contact@jmw.nz>
2021-09-19 10:36:54 +1200

    Updated Site and improved UX of scroll down

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -76,6 +76,10 @@ async function loadInputBox() {
 	}
 }
 
+function scrollDown() {
+	document.getElementById('section2').scrollIntoView(true);
+}
+
 function LandingContainer() {
 	loadInputBox()
 
@@ -95,7 +99,7 @@ function LandingContainer() {
 			</div>
 			<script>
 			</script>
-			<a className="revealContainer" href="#section2">
+			<a className="revealContainer" href="javascript:void(0);" onClick={scrollDown}>
 				<span>See More</span>
 				<svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 					<path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
