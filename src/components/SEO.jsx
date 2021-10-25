54cee8343a31df4af58ee7c5bc68e6b6e07f9d99
Jasper Miller-Waugh <contact@jmw.nz>
2021-10-25 21:12:53 +1300

    Integrated OG Images into SEO

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -7,7 +7,10 @@ export function SearchEngineOptimization (props) {
   console.log(props);
   let title = (props.titleExt !== undefined) ? 'Jasper M-W | '  + props.titleExt : 'Jasper M-W';
   console.log((props.description !== undefined) ? props.description : "Jasper Miller-Waugh's (Fallstop) portfolio");
-
+	let ogImage = props.ogImage;
+	if (typeof ogImage==="undefined") {
+		ogImage = '/OG_front_page.jpg';
+	}
   let description = (props.description !== undefined) ? props.description : "Jasper Miller-Waugh's (Fallstop) portfolio";
   return (
     <div>
@@ -24,7 +27,21 @@ export function SearchEngineOptimization (props) {
             lastName: 'Miller-Waugh',
             username: 'fallstop',
             gender: 'male',
-          }
+          },
+          images: [
+            {
+              url: 'https://jmw.nz/icons/icon-512x512.png',
+              width: 512,
+              height: 512,
+              alt: 'Fallstop Logo',
+            },
+            {
+              url: 'https://jmw.nz' + ogImage,
+              width: 1200,
+              height: 630,
+              alt: title,
+            },
+          ],
         }}
       />
     </div>
